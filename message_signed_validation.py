c58521c78274454cafe1a8e9b79e3a15e3055f79
Owen Synge <owen.synge@desy.de>
2011-03-30 11:55:22 +0200

    added simple command line application

---
+++ message_signed_validation.py
@@ -0,0 +1,18 @@
+import optparse
+import loadcanamespace
+
+def main():
+    p = optparse.OptionParser()
+    p.add_option('-m', '--message', action ='append', 
+        help='adds a message to be tested.')
+    p.add_option('-c', '--certs-dir', action ='store', 
+        help='Path of certificates dir',
+        default='/etc/grid-security/certificates/')
+    options, arguments = p.parse_args()
+    anchor =  loadcanamespace.view_trust_anchor()
+    anchor.update_ca_list(options.certs_dir)
+    for item in options.message:
+        anchor.validate_file(item)
+       
+if __name__ == "__main__":
+    main()
